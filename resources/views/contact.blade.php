5949162046c123652e925c9a2f16d897ef2b2f8f
Maho_Kururinpa <503steven10@gmail.com>
2026-04-09 01:48:07 +0800

    feat: add some basic test pages

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head lang="zh-TW">
+    <meta charset="utf-8">
+</head>
+<body>
+    <h1>神奇真步魔法靈~</h1>
+</body>
+</html>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head lang="zh-TW">
+    <meta charset="utf-8">
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>咕嚕靈波~</h1>
+</body>
+</html>
