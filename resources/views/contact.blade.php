a5bd1496645fbc53e32985a410df95393680d3a1
Maho_Kururinpa <503steven10@gmail.com>
2026-04-23 20:37:25 +0800

    feat: delete some old views and routes to start a new lesson

--- resources/views/contact.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head lang="zh-TW">
-    <meta charset="utf-8">
-</head>
-<body>
-    <h1>神奇真步魔法靈~</h1>
-</body>
-</html>
--- routes/web.php
+++ routes/web.php
@@ -6,24 +6,6 @@ Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/contact', function () {
-    return view('contact');
-})->name('contact');
-
 Route::get('/welcome', function () {
     return view('welcome');
 })->name('welcome');
-
-Route::get('/portfolio', function () {
-    return view('portfolio');
-})->name('portfolio');
-
-Route::prefix('portfolio')->group(function () {
-    Route::get('/company', function () {
-        return view('company');
-    })->name('company');
-
-    Route::get('/organization', function () {
-        return view('organization');
-    })->name('organization');
-});
