dcaf8c5eada32acdb238906b622f3f46f3b12983
Maho_Kururinpa <503steven10@gmail.com>
2026-04-24 01:15:04 +0800

    feat: test some basic post form and validation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,10 +2,20 @@
 <html>
 <head lang="zh-TW">
     <meta charset="utf-8">
-    @vite('resources/css/app.css')
+    {{-- @vite('resources/css/app.css') --}}
 </head>
 <body>
     <h1>咕嚕靈波~</h1>
-    <a href="{{ route('welcome') }}">Welcome</a>
+
+    <form action="{{route("formsubmitted")}}" method="POST">
+        @csrf
+        <label for="fullname">Full name:</label>
+        <input type="text" id="fullname" name="fullname" placeholder="Type your full name!" required>
+        <br>
+        <label for="email">E-mail:</label>
+        <input type="text" id="email" name="email" placeholder="Type your e-mail!" required>
+        <br>
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('home');
@@ -9,3 +10,12 @@ Route::get('/', function () {
 Route::get('/welcome', function () {
     return view('welcome');
 })->name('welcome');
+
+Route::post("/formsubmitted", function (Request $request) {
+    $validated = $request->validate([
+        'fullname' => 'required|min:3|max:30',
+        'email' => 'required|min:3|email',
+    ]);
+    
+    return "Your fullname is {$request->input('fullname')} and your email is {$validated['email']}.";
+})->name('formsubmitted');
